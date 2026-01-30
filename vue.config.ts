917af4067b5638c0b3125bd8248771b54f9ae0a1
sparrowstuff <damnagett@gmail.com>
2026-01-30 16:03:54 +0300

    fix vue.config.ts and prepare for deploy

--- vue.config.ts
+++ vue.config.ts
@@ -1,1 +1,6 @@
-module.exports = [publicPath: process.env.NODE_ENV === 'production' ? '/vk-marusya-final': '/']
+mmodule.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/vk-marusya-final/' // Обратите внимание на слеши
+      : '/',
+}
